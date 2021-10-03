31f7f9512d1841142090c408c652ee395f02c1e2
Richard Kang <hweechiat@gmail.com>
2021-10-03 06:06:32 +0000

    changing test functions and test cases

---
+++ factorial.py
@@ -0,0 +1,10 @@
+"""<Insert what this script does here"""
+
+def factorial(x):
+    """ this function returns the factorial of n"""
+    if x <= 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
---
+++ test_factorial.py
@@ -0,0 +1,14 @@
+from factorial import factorial 
+
+def test_case_1():
+    assert factorial(0) == 0
+    
+def test_case_2():
+    assert factorial(1) == 1
+
+def test_case_3():
+    assert factorial(2) == 2
+    
+def test_case_4():
+    assert factorial(3) == 6
+
