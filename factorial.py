c3d0ee6334253e3b5a13a38b98c369f8b1879485
Richard Kang <hweechiat@gmail.com>
2021-10-03 06:45:04 +0000

    fix bug and typo

--- factorial.py
+++ factorial.py
@@ -1,10 +1,11 @@
-"""<Insert what this script does here"""
+"""<insert what this script does here>"""
+
 
 def factorial(x):
-    """ this function returns the factorial of n"""
+    """this function returns the factorial of n"""
     if x <= 0:
         return 0
     elif x == 1:
         return 1
     else:
-        return x * factorial(x-1)
+        return x * factorial(x - 1)
--- makefile
+++ makefile
@@ -6,9 +6,8 @@ format:
 	black *.py
 	
 lint:
-	pylint --disable=R, C factorial.py
+	pylint --disable=R,C factorial.py
 	
 test:
 	python -m pytest -vv --cov=factorial test_factorial.py
 	
-all: install lint test 
